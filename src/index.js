a69a986648aec508cb86449020954bebc9d9cfc8
William Lachance <wlachance@mozilla.com>
2018-06-21 16:19:04 -0400

    Update to comply with airbnb/airbnb react linting rules
    This implies some changes in the code, for the better IMO.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import { render } from 'react-dom';
-import MetricsGraphics from './components/MetricsGraphics.js';
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+/* eslint react/jsx-filename-extension: [1, { "extensions": [".js", ".jsx"] }] */
+import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'metrics-graphics/dist/metricsgraphics.css';
+import React from 'react';
+import { render } from 'react-dom';
+import MetricsGraphics from './components/MetricsGraphics';
 
 const root = document.getElementById('root');
 
 const data = [
-  {'date': new Date('2014-01-01'), 'value': 100 },
-  {'date': new Date('2015-01-01'), 'value': 200 },
-  {'date': new Date('2016-01-01'), 'value': 250 },
-  {'date': new Date('2017-01-01'), 'value': 200 },
+  { date: new Date('2014-01-01'), value: 100 },
+  { date: new Date('2015-01-01'), value: 200 },
+  { date: new Date('2016-01-01'), value: 250 },
+  { date: new Date('2017-01-01'), value: 200 },
 ];
 
-render((
-  <MetricsGraphics
-  title="Line Chart"
-  description="This is a simple line chart."
-  data={data}
-  width={600}
-  height={200}
-  right={40}
-  x_accessor="date"
-  y_accessor="value" />
-), root);
+render(
+  (
+    <MetricsGraphics
+      title="Line Chart"
+      description="This is a simple line chart."
+      data={data}
+      width={600}
+      height={200}
+      right={40}
+      x_accessor="date"
+      y_accessor="value"
+    />
+  ), root,
+);
